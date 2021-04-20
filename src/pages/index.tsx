13050e115094ed3ba76fb647091c0b155e4d5654
rodolfoHOk <rodolfo.okino@gmail.com>
2021-04-20 15:40:57 -0300

    aula 1 e aula 2

---
+++ src/pages/index.tsx
@@ -0,0 +1,46 @@
+
+
+export default function Home(props) {
+  // SPA:
+  /*
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  },[]);
+  */
+
+  return (
+    <>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
+  );
+}
+
+// SSR:
+/*
+export async function getServerSideProps(){
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+    
+  return { 
+    props: { 
+      episodes: data
+    } 
+  };
+}
+*/
+
+// SSG:
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+    
+  return { 
+    props: { 
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  };
+}
